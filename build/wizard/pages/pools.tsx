29b74273b94ca4d16b6cbca8b9a11fb685550ff3
sponnet <stefaan@ponnet.com>
2023-05-11 20:58:52 +0200

    gunDB first steps

--- build/wizard/pages/pools.tsx
+++ build/wizard/pages/pools.tsx
@@ -1,16 +1,26 @@
 import type { NextPage } from "next";
-import { useState } from "react";
-import { usePools } from "../hooks/read/usePools";
+import { useState, useEffect } from "react";
+import useGunData from "../hooks/read/useGunData";
 
 const Home: NextPage = () => {
-    const [open, setOpen] = useState(false);
-    const { pools, createPool } = usePools();
+    const [visData, setVisData] = useState(false);
+    const { data, node,addData } = useGunData("pools1");
+
+    // useEffect(()=>{
+    //     console.log(`data client changed!`);
+    // },[data])
+
+    node?.once((data)=>{
+        console.log(`Data chagnedd`,data);
+        setVisData(data);
+    })
+
 
     return (
         <>
             <div>My Pools:</div>
-            <pre>{JSON.stringify(pools)}</pre>
-            <button onClick={() => { createPool({ name: "sponnet.eth pool" }) }}>create pool</button>
+            <pre>{JSON.stringify(visData,null,2)}</pre>
+            <button onClick={() => { addData({ name: "sponnet.eth pool" }) }}>create pool</button>
         </>
     );
 };
--- build/wizard/server_config.ts
+++ build/wizard/server_config.ts
@@ -3,7 +3,7 @@ const dev = true;
 export const server_config = dev ?
 {
   monitor_url: "http://localhost:9999",
-  gundb_url: "http://localhost:9998"
+  gundb_url: "http://localhost:9998/gun"
 }:{
   monitor_url: "https://frens.my.ava.do:9999",
   gundb_url: "http://frens.my.ava.do:9998"
